f0dfd630001df6a1dbd68db674dea3359a763686
FreddieMercy <junhao.zhang.freddie@gmail.com>
2020-12-22 23:43:32 -0800

    initial practice

---
+++ R Practice/src/Practice.R
@@ -0,0 +1,13 @@
+library(datasets) # load package (in this case, some built-in raw data)
+head(iris)
+
+?library
+?head
+?print
+ print(iris)
+ 
+head(iris, 1)
+library(pacman)
+p_load(datasets)
+
+
