4ad16c1b1450ad4eb9a89ec395c587722ab30873
FreddieMercy <junhao.zhang.freddie@gmail.com>
2020-12-27 23:36:03 -0800

    Practice Polynomial Regression

--- Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/R/data_preprocessing_template.R
+++ Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/R/data_preprocessing_template.R
@@ -1,16 +1,30 @@
 # Data Preprocessing Template
 
 # Importing the dataset
-dataset = read.csv('Data.csv')
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+# library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
 
-# Feature Scaling
+# Feature Scaling # Polynomial doesn't need feature scaling 
 # training_set = scale(training_set)
 # test_set = scale(test_set)
+
+## Simple Linear Regression
+
+lin_Reg = lm(Salary ~ ., 
+             data = dataset)
+
+## Polynomial Regression 
+
+dataset$level2 = dataset$Level^2
+
+poly_Reg = lm(Salary ~ ., 
+             data = dataset)
+summary(poly_Reg)
