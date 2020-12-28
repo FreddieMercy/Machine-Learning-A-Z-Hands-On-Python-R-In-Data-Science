848ccc07e34b65bbf804d2a0967e31c2c3e095ec
FreddieMercy <junhao.zhang.freddie@gmail.com>
2020-12-27 20:53:05 -0800

    Apply Backend Elimination

--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/R/multiple_linear_regression.R
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/R/multiple_linear_regression.R
@@ -26,3 +26,40 @@ regressor = lm(formula = Profit ~ ., # Profit ~ R.D.Spend + Administration + Mar
 
 # Predicting the Test set results
 y_pred = predict(regressor, newdata = test_set)
+
+# Backward Elimination
+
+# 1
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
+               data = dataset)
+
+#summary(regressor)
+
+# 2
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend,
+               data = dataset)
+# 3
+regressor = lm(formula = Profit ~ R.D.Spend + Marketing.Spend,
+               data = dataset)
+# 4
+regressor = lm(formula = Profit ~ R.D.Spend,
+               data = dataset)
+
+### Automatic Backward Elimination 
+backwardElimination <- function(x, sl) {
+  numVars = length(x)
+  for (i in c(1:numVars)){
+    regressor = lm(formula = Profit ~ ., data = x)
+    maxVar = max(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"])
+    if (maxVar > sl){
+      j = which(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"] == maxVar)
+      x = x[, -j]
+    }
+    numVars = numVars - 1
+  }
+  return(summary(regressor))
+}
+
+SL = 0.05
+dataset = dataset[, c(1,2,3,4,5)]
+backwardElimination(training_set, SL)
